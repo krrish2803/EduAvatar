4c82270b076b5d2f1ada9023bd1a7d64367b640c
krrish2803 <kyaduka2803@gmail.com>
2026-09-02 21:57:03 +0530

    fix: add edu-avatar.netlify.app to CORS

--- server.js
+++ server.js
@@ -95,6 +95,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:8080',
         'http://localhost:3001',
+        'https://edu-avatar.netlify.app',
         'https://eduavatar.netlify.app',
         process.env.RENDER_EXTERNAL_URL
     ].filter(Boolean)
